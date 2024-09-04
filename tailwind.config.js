9706cf20a16e9344487e0f36cce76d5a3c1e6bb3
Ilja <chylmanprosto@gmail.com>
2024-09-04 17:46:52 +0300

    feat: add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
   content: ['./source/**/*.{html,js}'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    container: {
+      padding: '32px',
+      center: true,
+    },
+    fontSize: {
+      base: ['16px', '150%'],
+      xl: ['32px', '137%'],
+    },
+    extend: {
+      colors: {
+        'hawkes-blue': '#D7DCE9',
+        'periwinkle': '#CED3E1',
+        'black-russian': '#141517',
+      },
+    },
   },
   plugins: [],
 }
