7509df2f39ec52a2fe2757d45cce6951adc20042
Ilja Afanasev <chylmanprosto@gmail.com>
2024-09-09 22:42:16 +0300

    feat: setting hide popover

--- source/js/modules/tippy/init-tippy.js
+++ source/js/modules/tippy/init-tippy.js
@@ -1,4 +1,4 @@
-import tippy, {followCursor} from 'tippy.js';
+import tippy from 'tippy.js';
 import initClipboard from '../clipboard/init-clipboard.js';
 
 const initTippySuccessfully = () => {
@@ -7,6 +7,7 @@ const initTippySuccessfully = () => {
     theme: 'white',
     arrow: false,
     placement: 'bottom',
+    delay: [null, 2000],
     popperOptions: {
       strategy: 'fixed',
     },
@@ -16,15 +17,18 @@ const initTippySuccessfully = () => {
           width: window.innerWidth,
           height: window.innerHeight,
           left: 0,
-          top: window.innerHeight-60,
+          top: window.innerHeight - 60,
         }),
         popperOptions: {
           strategy: 'fixed',
-        }
+        },
       });
-    }
+    },
+    onShown(instance) {
+      setTimeout(instance.hide, 1500);
+    },
   });
-}
+};
 
 const initTippyContent = () => {
   tippy('[data-tippy-content]', {
@@ -34,10 +38,11 @@ const initTippyContent = () => {
     theme: 'white',
     placement: 'bottom',
     arrow: true,
-    onShown() {
+    onShown(instance) {
       initClipboard();
       initTippySuccessfully();
-    }
+      instance.hideWithInteractivity('scroll');
+    },
   });
 };
 
